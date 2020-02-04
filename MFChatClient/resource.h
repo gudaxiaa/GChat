d6bf440024f980bd1884fea32c93e28e777d8c0c
gudaxia <2812470394@qq.com>
2020-02-04 14:03:51 +0800

    上传基线代码

---
+++ MFChatClient/resource.h
@@ -0,0 +1,32 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 MFChatClient.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFCHATCLIENT_DIALOG         102
+#define IDR_MAINFRAME                   128
+#define IDC_HISTORY_LIST                1000
+#define IDC_CLEAN_BTN                   1001
+#define IDC_CONNECT_BTN                 1002
+#define IDC_DISCONNECT_BTN              1003
+#define IDC_PORT_EDIT                   1004
+#define IDC_IPADDRESS                   1005
+#define IDC_EDIT2                       1006
+#define IDC_AUTOSEND_EDIT               1006
+#define IDC_AUTOSEND_RADIO              1007
+#define IDC_SENDMS_EDIT                 1008
+#define IDC_COLOR_COMBO                 1009
+#define IDC_SEND_BTN                    1010
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1011
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
